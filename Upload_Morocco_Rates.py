982ea92291cd8ed587753e63c3beb464c89aefd2
Michał Zawadzki <michalmzawadzki@gmail.com>
2017-03-18 22:53:39 +0100

    Add files via upload

--- Upload_Morocco_Rates.py
+++ Upload_Morocco_Rates.py
@@ -32,11 +32,11 @@ rates_MA.iloc[:,7] = rates_MA.iloc[:,7].div(rates_MA.iloc[:,8], axis=0)
 useless_cols = [x for x in range(rates_MA.shape[1]) if x not in [2, 3, 4, 7]]
 rates_MA.drop(rates_MA[useless_cols], axis=1, inplace=True) #rates_MA.drop(rates_MA.columns[useless_cols], axis=1, inplace=True)
 
-# extract the rates' effective date for output file and the file's name
+# extract the rates' effective date for output file and the file's name -- must use Excel's number format
 date = rates_MA.iloc[0,2]
 excel_date_format = (date - datetime.datetime(1899, 12, 31)).days + 1
 #rates_MA.iloc[:,2] = rates_MA.iloc[:,2].dt.strftime("%d-%m-%Y") #"%m/%d/%Y"
-rates_MA.iloc[:,2] = np.array(excel_date_format) * 11
+rates_MA.iloc[:,2] = np.array(excel_date_format)
 print(rates_MA)
 
 # file path + name of the file
